2ba7e292c361d694aff424606f972734bc43e7ed
Valentin Nourdin <valentin.nourdin@hotmail.fr>
2016-03-17 12:22:45 +0100

    renommage + début arbreToFich

--- src/main/scala/akinator/Akinator.scala
+++ src/main/scala/akinator/Akinator.scala
@@ -11,8 +11,8 @@ object Akinator {
   case class Question(q: String, oui: ABanimal, non: ABanimal) extends ABanimal
 
 
-  def fichierToABanimal(nomf: String): ABanimal = {
-    val l = Source.fromFile(nomf).getLines().toList
+  def fichierToABanimal(cheminVersFichier: String): ABanimal = {
+    val l = Source.fromFile(cheminVersFichier).getLines().toList
 
     def aux(l: List[String]): (ABanimal, List[String]) = l match {
       case Nil => throw new Exception("Un arbre binaire plein n'a pas de noeud vide")
@@ -22,9 +22,17 @@ object Akinator {
         (new Question(t.substring(3), abanimalG, abanimalD), listD)
       case t :: q => (new Animal(t), q)
     }
-    val (a, liste) = aux(l)
+    val (arbreResultat, liste) = aux(l)
 
-    a
+    arbreResultat
+  }
+
+  def ABanimalToFichier(cheminVersFichier: String, arbre: ABanimal): Unit = {
+    import java.io._
+    val writer = new FileWriter(new File("coucou.txt"))
+    writer.write("coucou\n")
+
+    writer.close()
   }
 
   def jeuSimple(a: ABanimal, it: Iterator[String]): Boolean = a match {
