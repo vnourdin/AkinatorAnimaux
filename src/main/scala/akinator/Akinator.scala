d20a8376fbcc239f6e3abbe13dcb01ecb40b22f4
Max DOLET <Max DOLET>
2016-03-20 14:49:53 +0100

    Patch JNSP ou JNSP comment faire

--- src/main/scala/akinator/Akinator.scala
+++ src/main/scala/akinator/Akinator.scala
@@ -143,7 +143,7 @@ object Akinator {
   def jeuApprentissage(arbre: ABanimal): ABanimal = {
     jeuApprentissage(arbre, Source.stdin.getLines)
   }
-
+ 
   def jeuSimpleJNSP(arbre: ABanimal, it: Iterator[String]): Boolean = {
     System.out.println("\nDans cette version du jeu, vous pouvez répondre \"x\" pour dire que vous ne connaissez pas la réponse à la question posée.")
     System.out.print("\nPensez à un animal - ")
